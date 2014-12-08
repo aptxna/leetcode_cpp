ca357f0bd91e6447648179518e6e8172534d37a0
nana <aptxna@gmail.com>
2014-12-08 05:47:16 -0600

    add file

---
+++ MaximumProductSubarray_n^2.h
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(int A[], int n) {
+        int maxp = A[0];
+        for (int i = 0; i < n; ++i) {
+            int temp = 1;
+            for (int j = i; j < n; ++j) {
+                temp *= A[j];
+                if (temp > maxp) maxp = temp;
+            }
+        }
+        return maxp;
+    }
+};
