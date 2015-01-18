7686c2290d700e55464053f81d98a23a7f197766
nana <aptxna@gmail.com>
2015-01-18 15:30:02 -0600

    add another way to solve

--- CompareVersionNumbers.h
+++ CompareVersionNumbers.h
@@ -37,3 +37,38 @@ public:
 		return res;
 	}
 };
+
+
+/*
+ * another way to solve this problem
+ */
+
+class CompareVersionNumbers {
+public:
+	int compareVersion(string version1, string version2) {
+		int i = 0, j = 0;
+		int num1 = 0, num2 = 0;
+		while (i < version1.size() || j < version2.size()) {
+			num1 = 0, num2 = 0;
+			while (i < version1.size() && version1[i] != '.') {
+				//string to int
+				//num1*10 is to move digit to left
+				//in ASCII code, '0' is 48
+				//minus '0' then trans char to int in decimal
+				num1 = num1 * 10 + version[i] - '0';
+				i++;
+			}
+			while (j < version2.size() && version2[j] != '.') {
+				num2 = num2 * 10 + version[j] - '0';
+				j++;
+			}
+			if (num1 > num2) return 1;
+			else if (num1 < num2) return -1;
+			else {
+				i++;
+				j++;
+			}
+		}
+		return 0;
+	}
+};
