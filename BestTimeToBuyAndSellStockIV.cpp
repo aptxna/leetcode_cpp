c82a3fd903d29a0dcf73cd605f7407d62eb66b18
nana <aptxna@gmail.com>
2015-04-13 19:48:13 -0500

    Best Time to Buy and Sell Stock IV

--- BestTimeToBuyAndSellStockIV.cpp
+++ BestTimeToBuyAndSellStockIV.cpp
@@ -71,11 +71,11 @@ class Solution {
 public:
     int maxProfit(int k, vector<int> &prices) {
         if (prices.empty()) return 0;
-        if (k >= prices.size()) return solveMaxProfit(prices);
+        if (k >= prices.size()) return quickSolve(prices);
         int g[k + 1] = {0};
         int l[k + 1] = {0};
-        for (int i = 0; i < prices.size() - 1; ++i) {
-            int diff = prices[i + 1] - prices[i];
+        for (int i = 1; i < prices.size(); ++i) {
+            int diff = prices[i] - prices[i-1];
             for (int j = k; j >= 1; --j) {
                 l[j] = max(g[j - 1] + max(diff, 0), l[j] + diff);
                 g[j] = max(g[j], l[j]);
@@ -83,13 +83,13 @@ public:
         }
         return g[k];
     }
-    int solveMaxProfit(vector<int> &prices) {
-        int res = 0;
+    int quickSolve(vector<int> &prices) {
+        int result = 0;
         for (int i = 1; i < prices.size(); ++i) {
             if (prices[i] - prices[i - 1] > 0) {
-                res += prices[i] - prices[i - 1];
+                result += prices[i] - prices[i - 1];
             }
         }
-        return res;
+        return result;
     }
 };
