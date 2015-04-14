893496bacf1b376aa2caf1a2adffa8d0c9411a12
nana <aptxna@gmail.com>
2015-04-13 22:41:31 -0500

    Factorial Trailing Zeroes 求n阶乘后缀0个数

--- FactorialTrailingZeroes.cpp
+++ FactorialTrailingZeroes.cpp
@@ -2,7 +2,13 @@
  * Factorial Trailing Zeros
  * given an integer n, return the number of trailing zeroes in n!
  * Note: your solution should be in logarithmic time complexity.
+ * 
+ * 返回n的阶乘后缀0的个数！
+ * 
+ * 2×5=10，2比5多，数5就可以了！
+ *
  */
+
 
  class FactorialTrailingZeroes {
  public:
