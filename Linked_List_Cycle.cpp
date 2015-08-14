ed3cb15686cb9e85c6cadc7cbf55f10fce02d13d
nana <aptxna@gmail.com>
2015-08-14 14:08:02 -0500

    Linked List Cycle

--- Linked_List_Cycle.cpp
+++ Linked_List_Cycle.cpp
@@ -12,7 +12,13 @@
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
+ *
+ * 用快慢指针或者hashset
+ * 把扫过的结点放入haset，重复则返回true
+ * 不过这里要求不用extra space，用快慢指针
+ * 慢指针每次向前一步，快指针每次向前两步，两指针相遇则返回true
  */
+ 
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
